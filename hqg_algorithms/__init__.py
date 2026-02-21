bca001254d606142f1a42b867032ddf1309df82d
Brendan Barnett <brendanabarnett@gmail.com>
2026-02-20 20:58:57 -0500

    feat: make cadence options more apparant to user + getters

--- hqg_algorithms/__init__.py
+++ hqg_algorithms/__init__.py
@@ -1,5 +1,5 @@
 """__init__.py"""
 from .strategy import Strategy
-from .types import Cadence, Slice, PortfolioView
+from .types import Cadence, Slice, PortfolioView, BarSize
 
-__all__ = ["Strategy", "Cadence", "Slice", "PortfolioView"]
+__all__ = ["Strategy", "Cadence", "Slice", "PortfolioView", "BarSize"]
