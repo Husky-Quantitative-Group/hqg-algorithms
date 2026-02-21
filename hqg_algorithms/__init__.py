f58f0a180d0b767a2bf75a2f9c71833d4fcb2834
Brendan Barnett <brendanabarnett@gmail.com>
2026-02-21 14:51:00 -0500

    chore: bring CallPhase top-level

--- hqg_algorithms/__init__.py
+++ hqg_algorithms/__init__.py
@@ -1,5 +1,5 @@
 """__init__.py"""
 from .strategy import Strategy
-from .types import Cadence, Slice, PortfolioView, BarSize
+from .types import Cadence, Slice, PortfolioView, BarSize, CallPhase
 
-__all__ = ["Strategy", "Cadence", "Slice", "PortfolioView", "BarSize"]
+__all__ = ["Strategy", "Cadence", "Slice", "PortfolioView", "BarSize", "CallPhase"]
